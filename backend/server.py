a979abdc628f6d649e58405edff0b4b04da67bb9
jflievanoh-ui <jflievanoh@gmail.com>
2025-08-06 16:58:53 -0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -6,3 +6,6 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # required by Render
     uvicorn.run(app, host="0.0.0.0", port=port)
+
+from routes import signaling_routes
+app.include_router(signaling_routes.router)
