649fb05607901e35d8878a450ff588111beb1f92
jflievanoh-ui <jflievanoh@gmail.com>
2025-08-07 18:27:11 -0500

    Update signaling_routes.py

--- backend/routes/signaling_routes.py
+++ backend/routes/signaling_routes.py
@@ -1,3 +1,4 @@
+# backend/routes/signaling_routes.py
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from typing import Dict, List
 
@@ -20,14 +21,22 @@ async def signaling_ws(websocket: WebSocket, room_id: str, participant_id: str):
             data = await websocket.receive_json()
 
             # Broadcast to all other participants in the room
-            for connection in connections[room_id]:
-                if connection != websocket:
-                    await connection.send_json({
-                        "from": participant_id,
-                        "data": data
-                    })
+            for connection in list(connections.get(room_id, [])):
+                if connection is not websocket:
+                    try:
+                        await connection.send_json({
+                            "from": participant_id,
+                            "data": data
+                        })
+                    except Exception:
+                        # If send fails, remove connection
+                        try:
+                            connections[room_id].remove(connection)
+                        except ValueError:
+                            pass
 
     except WebSocketDisconnect:
-        connections[room_id].remove(websocket)
-        if not connections[room_id]:
+        if room_id in connections and websocket in connections[room_id]:
+            connections[room_id].remove(websocket)
+        if room_id in connections and not connections[room_id]:
             del connections[room_id]
