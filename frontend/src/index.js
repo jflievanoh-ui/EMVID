2e3e8be64ec69e9d3775f65caedb8ced94608cc6
jflievanoh-ui <jflievanoh@gmail.com>
2025-08-21 05:24:42 -0500

    aaaa

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
+import { StudioProvider } from "./contexts/StudioContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <StudioProvider>
+        <App />
+      </StudioProvider>
+    </AuthProvider>
   </React.StrictMode>,
 );
